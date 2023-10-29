481e2932bd0b09899f52f42f711d25185a879717
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-10-29 21:14:30 +0100

    feat(product): Define availability choices
    Introduce the `AVAILABILITY_CHOICES` list, representing different availability states for products. Choices include 'Available,' 'Out of Stock,' 'Coming Soon,' and 'New.' This commit establishes a set of availability options for products within the system.

--- dipapp/models.py
+++ dipapp/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    AVAILABILITY_CHOICES = [
+        ('available', 'Available'),
+        ('out_of_stock', 'Out of Stock'),
+        ('coming_soon', 'Coming Soon'),
+        ('new', 'New'), 
+    ]
     product_name   = models.CharField(max_length=200, unique=True)
     slug           = models.SlugField(max_length = 200, unique= True)
     description    = models.TextField(max_length = 500, blank=True)
