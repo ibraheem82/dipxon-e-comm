e6e29e1e89e4277a9b8b0d51df5edae0421afd06
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-10-29 21:00:15 +0100

    feat(product): Define Product model
    Introduce the `Product` model with fields for product name, slug, description, price, discount price, images, stock, and availability status. The model includes methods to calculate and save the discounted price based on the provided discount percentage. This commit establishes the foundation for managing product information in the system.

--- dipapp/models.py
+++ dipapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name   = models.CharField(max_length=200, unique=True)
+    slug           = models.SlugField(max_length = 200, unique= True)
+    description    = models.TextField(max_length = 500, blank=True)
+    price          = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.PositiveIntegerField()
+    images         = models.ImageField(upload_to = 'photos/products')
+    stock          = models.IntegerField()
+    is_available   = models.BooleanField(default=True)
+    # category      = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date   = models.DateTimeField(auto_now_add=True)
+    modified_date  = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        # Calculate discounted price before saving
+        if self.discount_price > 0:
+            discount = self.price * (self.discount_price / 100)
+            self.discounted_price = self.price - discount
+        else:
+            # If discount is not provided, set discounted price same as regular price
+            self.discounted_price = self.price
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product_name
