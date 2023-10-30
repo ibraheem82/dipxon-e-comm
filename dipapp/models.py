d5c8f115f8d6c25811668d59734072d79117f77e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-10-30 11:22:16 +0100

    feat(product): Enhance save methods in Product and ProductImage models
    In the `Product` model, improve the `save` method to handle cases where the `discount_price` is `None` or not provided, ensuring proper calculation and storage of the discounted price. Additionally, update the `__str__` method for better representation.
    
    Introduce the `ProductImage` model with a `MAX_IMAGES_PER_PRODUCT` constant to limit the number of images associated with each product. Enhance the `save` method in the `ProductImage` model to check the existing number of images and prevent saving if the maximum limit is reached. Resize and save images to maintain consistency. This commit enhances the saving functionality in both models for improved data handling.

--- dipapp/models.py
+++ dipapp/models.py
@@ -17,8 +17,7 @@ class Product(models.Model):
     slug           = models.SlugField(max_length = 200, unique= True)
     description    = models.TextField(max_length = 500, blank=True)
     price          = models.DecimalField(max_digits=10, decimal_places=2)
-    discount_price = models.PositiveIntegerField()
-    images         = models.ImageField(upload_to = 'photos/products')
+    discount_price = models.PositiveIntegerField(blank=True, null=True, default=0)
     stock          = models.IntegerField()
     is_available   = models.CharField(
         max_length=20,
@@ -31,44 +30,49 @@ class Product(models.Model):
     
     
     def save(self, *args, **kwargs):
-        # Calculate discounted price before saving
-        if self.discount_price > 0:
+        # Check if discount_price is not None before comparison
+        if self.discount_price is not None and self.discount_price > 0:
             discount_percentage = Decimal(self.discount_price) / 100
             discount = self.price * discount_percentage
             self.discounted_price = self.price - discount
         else:
-            # If discount is not provided, set discounted price the same as the regular price
+            # If discount is not provided or is None, set discounted price the same as the regular price
             self.discounted_price = self.price
 
         # Call the original save method
         super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.product_name        
+        
+class ProductImage(models.Model):
+    MAX_IMAGES_PER_PRODUCT = 2
 
-        # Resize and save image if it exists
-        if self.images and self.images.name:
-            product_name = self.product_name.replace(" ", "_").lower()
-
-            # Ensure the original image file exists before proceeding
-            if os.path.exists(self.images.path):
-                # Open the original image
-                img = Image.open(self.images.path)
-
-                # Resize the image
-                resized_img = img.resize((500, 666), resample=Image.LANCZOS)
-
-                # Use the product name as part of the filename
-                base_filename = f"{product_name}_image.png"
-
-                # Save the resized image in the same directory as the original image
-                output_path = os.path.join("media", "photos", "products", base_filename)
-                resized_img.save(output_path, "PNG")
-
-                # Update the images field with the new filename
-                self.images.name = os.path.join("photos", "products", base_filename)
-
-                # Call the original save method again to update the model with the new image filename
-                super().save(*args, **kwargs)
-
-
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image   = models.ImageField(upload_to='photos/products')
 
     def __str__(self):
-        return self.product_name
+        return f"Image for {self.product.product_name}"
+
+    def save(self, *args, **kwargs):
+        # Check the number of existing images for the product
+        existing_images_count = self.product.images.count()
+
+        if existing_images_count >= self.MAX_IMAGES_PER_PRODUCT:
+            # If the maximum number of images is reached, don't save the new image
+            return
+
+        super().save(*args, **kwargs)
+
+        if self.image and self.image.name:
+            product_name = self.product.product_name.replace(" ", "_").lower()
+            base_filename = f"{product_name}_image_{self.pk}.png"
+
+            img = Image.open(self.image.path)
+            resized_img = img.resize((500, 666), resample=Image.LANCZOS)
+
+            output_path = os.path.join("media", "photos", "products", base_filename)
+            resized_img.save(output_path, "PNG")
+
+            self.image.name = os.path.join("photos", "products", base_filename)
+            super().save(*args, **kwargs)
