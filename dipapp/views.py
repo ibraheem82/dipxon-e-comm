0a81c64bd6fc2a960a4a29ccf9466f2bccbc1796
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-02 09:27:08 +0100

    all updated.. for now...

--- dipapp/views.py
+++ dipapp/views.py
@@ -3,5 +3,5 @@ from .models import Product
 
 def home(request):
     # Get the first four products with their images
-    top_three_products = Product.objects.prefetch_related('images').all()[:4]
-    return render(request, 'dipapp/home.html', {'top_three_products': top_three_products})
+    top_four_products = Product.objects.prefetch_related('images').all()[:4]
+    return render(request, 'dipapp/home.html', {'top_four_products': top_four_products})
