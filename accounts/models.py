e1f74d02a8e509b23e05e4db06f0a6987664d901
ibraheem705 <ibraheemomikunle16@gmail.com>
2023-11-14 13:48:17 +0100

    uuid was imported...

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+import uuid
+
 
 # * Creation of user instance
 class MyAccountManager(BaseUserManager):
