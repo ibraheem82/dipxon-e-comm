c142c050fe707735b69434aab3aeea1be2536124
ibraheem705 <ibraheemomikunle16@gmail.com>
2023-11-14 18:33:16 +0100

    Override Django admin panel display for the [category] model:
    - Set verbose names for improved readability in the admin panel.
    - Define a custom URL retrieval method, 'get_url', for use in header templates.
    - The method 'get_url' generates the URL for a specific category using the 'products_by_category' slug.

--- category/models.py
+++ category/models.py
@@ -12,3 +12,26 @@ class Category(models.Model):
     slug = models.SlugField(max_length = 100,unique=True)
     description = models.TextField(max_length = 255, blank=True)
     cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+
+
+
+
+        
+    # ===> overiding the name [category] in the django admin panel
+    # ===> the name will show under our [app table] in the admin panel
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+        
+    # ===> this will bring us the url of a particular category
+    # ===> 'get_url' will be use in the loop in the header templates
+    def get_url(self):
+        # ===> 'products_by_category' is the name of the category slug used in the url
+        return reverse('products_by_category', args=[self.slug])    
+    
+    
+    
+    def __str__(self):
+        return self.category_name
