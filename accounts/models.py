82319b6bb6103b5e4235cea4da75d12add4b65a7
ibraheem705 <ibraheemomikunle16@gmail.com>
2023-11-14 13:48:52 +0100

    added uuid field to your Account model:

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 import uuid
-
+from uuid import UUID
 
 # * Creation of user instance
 class MyAccountManager(BaseUserManager):
