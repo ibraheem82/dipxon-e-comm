bbf409664411ab8757ae0b74be0425e43684b033
ibraheem705 <ibraheemomikunle16@gmail.com>
2023-11-14 13:49:41 +0100

    id = UUIDField(primary_key=True, default=uuid.uuid4)

--- accounts/models.py
+++ accounts/models.py
@@ -56,6 +56,7 @@ class MyAccountManager(BaseUserManager):
     
     # * creating a custom user model..
 class Account(AbstractBaseUser):
+    id                  = UUIDField(primary_key=True, default=uuid.uuid4)
     first_name          = models.CharField(max_length=50)
     last_name           = models.CharField(max_length=50)
     username            = models.CharField(max_length=50, unique=True)
