74be79432e82f758ffe11dec9a1756a55b1d4168
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-15 17:23:56 +0100

    "Optimize home view: Retrieve all products and categories efficiently, while dynamically populating displayed subcategories using a set for improved performance."

--- dipapp/views.py
+++ dipapp/views.py
@@ -4,5 +4,19 @@ from .models import Product, Category
 def home(request):
     products = Product.objects.all()
     categories = Category.objects.all()
-    context = {'products': products, 'categories': categories}
+
+    # Create a set to track displayed subcategories
+    displayed_subcategories = set()
+
+    # Iterate through categories and subcategories to populate the set
+    for category in categories:
+        for subcategory, _ in category.SUBCATEGORY_CHOICES:
+            displayed_subcategories.add(subcategory)
+
+    context = {
+        'products': products,
+        'categories': categories,
+        'displayed_subcategories': displayed_subcategories,
+    }
+
     return render(request, 'dipapp/home.html', context)
