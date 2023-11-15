2376d26e3a1a9ca5a20bcd794c38931afee1925f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-15 17:44:56 +0100

    "Enhance home view: Utilize a custom Django template filter 'remove_from_list' to efficiently filter out specific elements from a list. Simplify subcategory extraction for improved readability and maintainability."

--- dipapp/views.py
+++ dipapp/views.py
@@ -2,22 +2,18 @@ from django.shortcuts import render
 from .models import Product, Category
 
 
+from django import template
+
+register = template.Library()
+
+@register.filter(name='remove_from_list')
+def remove_from_list(value, arg):
+    return [item for item in value if item != arg]
+
 def home(request):
     products = Product.objects.all()
     categories = Category.objects.all()
-
-    # Create a set to track displayed subcategories
-    displayed_subcategories = set()
-
-    # Iterate through categories and subcategories to populate the set
-    for category in categories:
-        for subcategory, _ in category.SUBCATEGORY_CHOICES:
-            displayed_subcategories.add(subcategory)
-
-    context = {
-        'products': products,
-        'categories': categories,
-        'displayed_subcategories': displayed_subcategories,
-    }
-
+    displayed_subcategories = [item[0] for category in categories for item in category.SUBCATEGORY_CHOICES]
+    context = {'products': products, 'categories': categories, 'displayed_subcategories': displayed_subcategories}
     return render(request, 'dipapp/home.html', context)
+
