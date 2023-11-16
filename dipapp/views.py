61212e4ab6267fa89b2684365ebdeff0afc79458
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-16 09:03:54 +0100

    "Optimize home view: Improve subcategory extraction for increased efficiency and readability. Create and pass a concise context dictionary to render the 'home.html' template."

--- dipapp/views.py
+++ dipapp/views.py
@@ -4,16 +4,19 @@ from .models import Product, Category
 
 from django import template
 
-register = template.Library()
-
-@register.filter(name='remove_from_list')
-def remove_from_list(value, arg):
-    return [item for item in value if item != arg]
-
 def home(request):
     products = Product.objects.all()
     categories = Category.objects.all()
-    displayed_subcategories = [item[0] for category in categories for item in category.SUBCATEGORY_CHOICES]
-    context = {'products': products, 'categories': categories, 'displayed_subcategories': displayed_subcategories}
-    return render(request, 'dipapp/home.html', context)
 
+    # Calculate the displayed subcategories
+    displayed_subcategories = [item[0] for category in categories for item in category.SUBCATEGORY_CHOICES]
+
+    # Create the context dictionary
+    context = {
+        'products': products,
+        'categories': categories,
+        'displayed_subcategories': displayed_subcategories,
+    }
+
+    # Render the template with the context data
+    return render(request, 'dipapp/home.html', context)
