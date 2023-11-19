6200d543f9a24a4736b756aa301b75c847b1dc6e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-19 20:50:43 +0100

    mailing path done......

--- dipxon/settings.py
+++ dipxon/settings.py
@@ -165,6 +165,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
