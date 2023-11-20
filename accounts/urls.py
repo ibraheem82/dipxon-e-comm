1474a9267b54bc8aff09494d0ec24dac208bb321
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-20 14:50:18 +0100

    feat: Implement IP-based user country detection
    Introduce functionality to determine the user's country based on their IP address using the ipstack API. This enables dynamic selection of currency symbols based on the detected country. Also, handle exceptions and provide appropriate default values for cases where IP information is unavailable or errors occur.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginUser, name="login"),
-    path('logout/', views.logoutUser, name="logout"),
+    # path('login/', views.loginUser, name="login"),
+    # path('logout/', views.logoutUser, name="logout"),
     path('register/', views.register, name='register')
 ]
