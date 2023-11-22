0d473fd5d66c965d508b529168c79fb17431efe5
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-22 01:13:50 +0100

    Add URL path for login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginUser, name="login"),
+    path('login/', views.login_view, name="login"),
     # path('logout/', views.logoutUser, name="logout"),
     path('register/', views.register, name='register'),
     path("activate/<uidb64>/<token>/", views.activate, name='activate'),
