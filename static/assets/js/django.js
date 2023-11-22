1baa21f2ddae3d8cbc48cc4aafa73b99557e5735
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-22 03:29:39 +0100

    Add add to cart functionality

--- static/assets/js/django.js
+++ static/assets/js/django.js
@@ -1,30 +1,34 @@
-function addToCart(productId) {
-    fetch(`/products/${productId}/add-to-cart/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": getCookie("csrftoken"),
-      },
+function addToCart(productId, event) {
+    event.preventDefault();
+    console.log('Adding to cart:', productId);  // Add this line for debugging
+    fetch(`/products/${productId}/add_to_cart/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": getCookie("csrftoken"),
+        },
     })
-      .then((response) => response.json())
-      .then((data) => {
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Response:', data);  // Add this line for debugging
         if (data.success) {
-          alert(`${data.product_name} has been added to your cart!`);
-          // Update cart counter or UI elements here
+            alert(`${data.product_name} has been added to your cart!`);
+            // Update cart counter or UI elements here
         } else {
-          console.error("Error adding product to cart:", data.error);
+            console.error("Error adding product to cart:", data.error);
         }
-      })
-      .catch((error) => {
+    })
+    .catch((error) => {
         console.error("Error during AJAX request:", error);
-      });
-  }
-  
-  // Helper function to retrieve CSRF token
-  function getCookie(name) {
+    });
+}
+
+// Helper function to retrieve CSRF token
+function getCookie(name) {
     const cookieValue = document.cookie.match(
-      new RegExp(`(^|; )${name}=([^;]*)`)
+        new RegExp(`(^|; )${name}=([^;]*)`)
     );
     return cookieValue ? decodeURIComponent(cookieValue[2]) : "";
-  }
-  
+}
+
+console.log('Testing');
