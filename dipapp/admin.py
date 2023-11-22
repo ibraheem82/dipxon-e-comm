c46802957c701787b67e1f001c9ae8eb8d293017
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-22 01:49:26 +0100

    Enhance admin site for Cart and CartItem models with custom display fields

--- dipapp/admin.py
+++ dipapp/admin.py
@@ -12,5 +12,15 @@ class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline]
     
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Cart)
-admin.site.register(CartItem)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'date_added')
+
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
+
+
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
