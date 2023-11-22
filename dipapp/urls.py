e86ffc4f616c4900ac0a3a07d46f1706b1c11470
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-22 03:39:21 +0100

    done...

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -3,6 +3,6 @@ from . import views
     
     
 urlpatterns = [
-    path('', views.home, name="home")
-    path('<int:product_id>/add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('', views.home, name="home"),
+    path('products/<int:product_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
 ]
