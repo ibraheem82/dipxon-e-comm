506f5cb7db9d3894ad0545fb5548914a009c49b5
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-23 08:41:21 +0100

    add url for shop page...

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -4,6 +4,7 @@ from . import views
     
 urlpatterns = [
     path('', views.home, name="home"),
+    path('shop/', views.shop, name="shop"),
     path('products/<int:product_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
     path('get_cart_count/', views.get_cart_count, name='get_cart_count'),
 ]
