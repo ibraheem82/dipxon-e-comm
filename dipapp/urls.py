6b43a84db7ce2ff40428c62ec164d8b64b18b6df
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-11-26 16:23:44 +0100

    ....

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('products/<int:product_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
     path('get_cart_count/', views.get_cart_count, name='get_cart_count'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-
 ]
