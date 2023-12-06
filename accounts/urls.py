b6a90d3f532fc776f41885492456f8e9c2ceaf91
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-06 21:30:47 +0100

    urls for the accounts app...

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name="login"),
+    # path('login/', views.login_view, name="login"),
     # path('logout/', views.logoutUser, name="logout"),
-    path('register/', views.register, name='register'),
-    path("activate/<uidb64>/<token>/", views.activate, name='activate'),
+    # path('register/', views.register, name='register'),
+    # path("activate/<uidb64>/<token>/", views.activate, name='activate'),
 ]
