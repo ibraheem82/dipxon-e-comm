65fcfccfb253c859da6f16a0945eaec51e199bbb
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-07 18:41:31 +0100

    slug and it related was removed...

--- category/models.py
+++ category/models.py
@@ -2,21 +2,14 @@ from django.db import models
 
 # Create your models here.
 from django.urls import reverse
-from django.utils.text import slugify
 
 # Create your models here.
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length = 100,unique=True, blank=True)
-    description = models.TextField(max_length = 255, blank=True)
-    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.category_name)
-        super().save(*args, **kwargs)
+    description   = models.TextField(max_length = 255, blank=True)
+    cat_image     = models.ImageField(upload_to='photos/categories', blank=True)
 
         
     # ===> overiding the name [category] in the django admin panel
