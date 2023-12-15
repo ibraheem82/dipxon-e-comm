37c8a169967ff2d43fd5e32e5d13dfd703d5e84b
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-15 12:01:04 +0100

    product ,  adding to cart part was done...

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views 
-from .views import ProductDetailView, ProductsByCategoryView
+from .views import ProductDetailView, ProductsByCategoryView, add_to_cart
     
     
 urlpatterns = [
     path('', views.home, name="home"),
     path('shop/', views.shop, name="shop"),
-    path('products/<int:product_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
-    path('get_cart_count/', views.get_cart_count, name='get_cart_count'),
     path('products_by_category/<uuid:category_id>/', ProductsByCategoryView.as_view(), name='products_by_category'),
     path('product/<uuid:product_id>/', ProductDetailView.as_view(), name='product_details'),
+    path('products/<int:product_id>/add-to-cart/', add_to_cart, name='add_to_cart'),
 ]
