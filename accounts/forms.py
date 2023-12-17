2029b6f2889fb7c65f1e840b433995d8d415ac66
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-17 19:42:29 +0100

    feat: Implement RegistrationForm and CustomLoginForm
    This commit introduces two Django forms, RegistrationForm and CustomLoginForm, for user registration and login. RegistrationForm includes fields for first name, last name, phone number, email, password, and password confirmation. It also includes form validation to ensure that the entered password and confirmation match. CustomLoginForm is designed for email and password input with appropriate placeholders and form control classes. Additionally, the forms' Bootstrap attributes are customized for improved aesthetics.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,2 +1,66 @@
 from django import forms
-# from . models import Account, UserProfile
+from django.contrib.auth.models import User
+from . models import Account
+
+
+
+class RegistrationForm(forms.ModelForm):
+    # ===> how we want our password input fields to be
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class' : 'form-control'
+    }))
+   
+   
+   
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'
+    }))
+   
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        
+        
+            # ===> we want to checking if the password the user are entering match or not.
+    def clean(self):
+        # ===> the 'super' class will allow you to change the way it is saving or change the way the class is been saved
+        # ===> we are modifying the super class.
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!❗❌"
+            )
+        
+        
+        
+        # ===> we are modifying our class so that we can be able to edit the boostrap of our forms
+        # ===> we are overiding the functionality of the form
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        # ===> we are setting the placeholder for each fields
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        # ===> we are looping through all the fields we have
+        for field in self.fields:
+            # ===> and this will assign all the attribute class to the fields we have looped through earlier
+            self.fields[field].widget.attrs['class'] = 'form-control'
+    
+    
+    
+class CustomLoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(attrs={
+        'placeholder': 'Enter Email',
+        'class': 'form-control'
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control'
+    }))
