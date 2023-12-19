7693c54c50272a3b4fccbfc89733915fcbb6f15f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-19 06:17:49 +0100

    model was restructured...

--- accounts/models.py
+++ accounts/models.py
@@ -6,30 +6,19 @@ import uuid
 # Create your models here.
 
 class Account(models.Model):
-    user                =  models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user                = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     first_name          = models.CharField(max_length=50)
     last_name           = models.CharField(max_length=50)
     email               = models.EmailField(max_length = 100, unique=True)
     phone_number        = models.CharField(max_length=50)
-    
     date_joined         = models.DateTimeField(auto_now_add=True)
     last_login          = models.DateTimeField(auto_now_add=True)
+    address_line_1      = models.CharField(blank = True, max_length = 100)
+    address_line_2      = models.CharField(blank = True, max_length = 100)
+    city                = models.CharField(blank=True, max_length=20)
+    state               = models.CharField(blank=True, max_length=20)
+    country             = models.CharField(blank=True, max_length=20)
     id                  =  models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
-
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-
-    
-    
-class UserProfile(models.Model):
-    user           = models.OneToOneField(Account, on_delete=models.CASCADE)
-    address_line_1 = models.CharField(blank = True, max_length = 100)
-    address_line_2 = models.CharField(blank = True, max_length = 100)
-    city           = models.CharField(blank=True, max_length=20)
-    state          = models.CharField(blank=True, max_length=20)
-    country        = models.CharField(blank=True, max_length=20)
-    id             =  models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    
-    def __str__(self):
-        return self.user.first_name
