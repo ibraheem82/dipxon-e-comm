493f284e09dd7361250f8db3d1bbecf2b191d79f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-20 20:17:25 +0100

    registration view done...

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate, logout
 # Create your views here.
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import RegistrationForm, CustomLoginForm
-from django.core.exceptions import ValidationError
+from .forms import RegistrationForm
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 # ========> Login View <========
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import render, redirect
 import logging
-from .models import Account, UserProfile
+from .models import Account
+from django.contrib.sites.shortcuts import get_current_site
 
 # Create a logger instance
 logger = logging.getLogger(__name__)
@@ -64,25 +69,39 @@ def registerUser(request):
     if request.method == 'POST':
         form  = RegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            # ! convert the [username] to lowercase.
-            user.email = user.email.lower()
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            
+            username = email.split("@")[0]
+            
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
             user.save()
-            messages.success(request, 'User account was created!')
             
+            current_site = get_current_site(request)
             
-            
-            login(request, user)
-            
-            
-            # return redirect('profiles')
+            mail_subject = 'Please activate your account'
 
-            # * When the user create thier account they should be redirected to the shop.
-            return redirect('login')
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+            
+            to_email = email
+            # ===> 
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
 
+            return redirect('/accounts/login/?command=verification=' + email)
 
         else:
-            messages.error(request, 'An error has occured during registration')
+            form = RegistrationForm()
+
     context = {'page' : page, 'form' : form}
     return render(request, 'accounts/login_register.html', context)
 
