00b628ebf70acf1a7aa69d36238ca588b4ac7ffe
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-21 13:06:03 +0100

    updated...

--- dipxon/settings.py
+++ dipxon/settings.py
@@ -149,6 +149,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
     "site_title": "DipXon Drips Admin",
