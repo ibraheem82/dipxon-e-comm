51a9497a8c04ab4dfa171fce6f5c8dd9f36e42d6
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-22 17:02:51 +0100

    url part done...

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-from .views import ProductDetailView, ProductsByCategoryView
+from .views import ProductDetailView, ProductsByCategoryView, add_to_cart
     
     
 urlpatterns = [
@@ -8,5 +8,5 @@ urlpatterns = [
     path('shop/', views.shop, name="shop"),
     path('products_by_category/<uuid:category_id>/', ProductsByCategoryView.as_view(), name='products_by_category'),
     path('product/<uuid:product_id>/', ProductDetailView.as_view(), name='product_details'),
-    # path('products/<uuid:product_id>/add-to-cart/', add_to_cart, name='add_to_cart'),
+    path('products/<uuid:product_id>/add-to-cart/', add_to_cart, name='add_to_cart'),
 ]
