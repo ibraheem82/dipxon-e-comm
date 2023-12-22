b65bfe81258a4ff77784e0b53a7e5cff4da86439
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-12-22 17:22:50 +0100

    feat: Implement views for home, product detail, and products by category
    This commit introduces three views: home, ProductDetailView, and ProductsByCategoryView, along with an 'add_to_cart' function. The 'home' view fetches a limited number of products and categories and renders them in the home template. 'ProductDetailView' displays detailed information about a specific product. 'ProductsByCategoryView' lists products belonging to a particular category. The 'add_to_cart' function handles the addition of products to the user's shopping cart, considering quantity validation and updating the cart accordingly.

--- dipapp/views.py
+++ dipapp/views.py
@@ -11,8 +11,9 @@ from .models import Cart, CartItem, Product
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
-from django.contrib.messages.views import SuccessMessageMixin
-from accounts.models import Account
+
+from django.core.exceptions import ObjectDoesNotExist
+from .utils import validate_quantity
 
 
 def home(request):
@@ -50,54 +51,55 @@ class ProductsByCategoryView(View):
 
 
 
-# def add_to_cart(request, product_id):
-#     try:
-#         product = Product.objects.get(pk=product_id)
+def add_to_cart(request, product_id):
+    try:
+        product = Product.objects.get(pk=product_id)
+        
+        # Retrieve the existing cart associated with the user (if any)
+        cart_item = CartItem.objects.filter(user=request.user).first()
+        cart = cart_item.cart if cart_item else None
 
-#         # Get or create cart based on customer
-#         cart, created = Cart.objects.get_or_create(user=customer, completed=False)
+        # Create a new cart if it doesn't exist
+        if not cart:
+            cart = Cart.objects.create()
+            CartItem.objects.create(user=request.user, cart=cart,  product_id=product.product_id)
 
-#         # Get desired quantity from form
-#         quantity = int(request.POST.get('quantity', 1))
+        # Get desired quantity from form
+        quantity = int(request.POST.get('quantity', 1))
 
-#         # Check if product is available in sufficient quantity
-#         if product.stock < quantity:
-#             return JsonResponse({
-#                 'success': False,
-#                 'message': f"Insufficient stock for {product.product_name}. Only {product.stock} available."
-#             })
+        # Check if product is available in sufficient quantity
+        # Validate quantity
+        error_message = validate_quantity(product, quantity)
+        if error_message:
+            return JsonResponse({'success': False, 'message': error_message})
 
-#         # Check if product is already in the cart
-#         cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+        # Check if product is already in the cart
+        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
 
-#         # Update quantity if already in cart
-#         if not created:
-#             cart_item.quantity += quantity
-#         else:
-#             cart_item.quantity = quantity
 
-#         cart_item.save()
+        cart_item.quantity += quantity
+        cart_item.save()
 
-#         # Prepare successful response data
-#         data = {
-#             'success': True,
-#             'message': f"{quantity} {product.product_name}(s) added to your cart.",
-#             'cart_quantity': cart_item.quantity,  # Include updated cart quantity for the product
-#         }
+        # Prepare successful response data
+        data = {
+            'success': True,
+            'message': f"{quantity} {product.product_name}(s) added to your cart.",
+            'cart_quantity': cart_item.quantity,  # Include updated cart quantity for the product
+        }
 
-#         return JsonResponse(data, safe=True)
+        return JsonResponse(data, safe=True)
 
-#     except ObjectDoesNotExist:
-#         return JsonResponse({
-#             'success': False,
-#             'message': "Product not found."
-#         })
+    except ObjectDoesNotExist:
+        return JsonResponse({
+            'success': False,
+            'message': "Product not found."
+        })
 
-#     except ValueError as ve:
-#         return JsonResponse({
-#             'success': False,
-#             'message': f"Invalid quantity entered: {ve}"
-#         })
+    except ValueError as ve:
+        return JsonResponse({
+            'success': False,
+            'message': f"Invalid quantity entered: {ve}"
+        })
 
 
 
