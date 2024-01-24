a7d4bbc9a217a958777a3f629ccf2b5ac0d51b4b
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-24 22:18:24 +0100

    register route was done. and linked to it view...

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@ from django import views
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('login/', views.loginUser, name="login"),
-    path('logout/', views.logoutUser, name="logout"),
-    path('register/', views.registerUser, name='register'),
-    path("activate/<uidb64>/<token>/", views.activate, name='activate'),
+#     path('login/', views.loginUser, name="login"),
+#     path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name='register')
+#     path("activate/<uidb64>/<token>/", views.activate, name='activate'),
 ]
