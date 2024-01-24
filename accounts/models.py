48e2338069b989718128d6de9eca4dcea5e0ffce
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-24 09:56:30 +0100

    bio field added...

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique = True)
     username = models.CharField(max_length = 100)
+    bio = models.CharField(max_length = 100)
 
 
     USERNAME_FIELD = "email"
