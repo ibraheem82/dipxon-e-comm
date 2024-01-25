c9c42dd4f0dc9bbdd73bb397a6cf7884f2cce662
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-25 11:59:46 +0100

    category app was deleted....

--- dipxon/settings.py
+++ dipxon/settings.py
@@ -49,8 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    # 'dipapp',
-    # 'category',
+    'dipapp',
     'django.contrib.humanize'
 ]
 
