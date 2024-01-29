f258e082a75a8a636218cf961b9ffaa6acd28305
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-29 16:47:33 +0100

    admin panel stuff was done...

--- dipapp/admin.py
+++ dipapp/admin.py
@@ -1,32 +1,62 @@
 from django.contrib import admin
 from dipapp.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, WishList, Address
 
-from
+# from
 # Register your models here.
 
 
-import admin_thumbnails
-# Register your models here.
+# import admin_thumbnails.
 
-class ProductImageInline(admin.TabularInline):
-    model = ProductImage
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
     extra = 1  # Number of empty forms to display for adding new images
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'price', 'stock', 'modified_date', 'category', 'is_available')
-    prepopulated_fields  = {'slug': ('product_name',)}
-    inlines = [ProductImageInline]
+    inlines = [ProductImagesAdmin]
+    list_display = ['user','title', 'product_image', 'price', 'featured','product_status']
+
+#     
     
+# admin.site.register(Product, ProductAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category_image']
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['title', 'vendor_image']
+    
+    
+    
+class CartOrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'price', 'paid_status', 'order_date', 'product_status']
+    
+    
+    
+class CartOrderItemsAdmin(admin.ModelAdmin):
+    list_display = ['order', 'invoice_no', 'item', 'image', 'qty', 'price', 'total']
+    
+    
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'review', 'rating']
+    
+    
+    
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'date']
+    
+    
+    
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address', 'status']
+
+
+
+
 admin.site.register(Product, ProductAdmin)
-class CartAdmin(admin.ModelAdmin):
-    list_display = ('cart_id', 'date_added')
-
-
-class CartItemAdmin(admin.ModelAdmin):
-    list_display = ('product', 'cart', 'quantity')
-
-
-
-
-admin.site.register(Cart, CartAdmin)
-admin.site.register(CartItem, CartItemAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(CartOrder, CartOrderAdmin)
+admin.site.register(CartOrderItems, CartOrderItemsAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
+admin.site.register(WishList, WishlistAdmin)
+admin.site.register(Address, AddressAdmin)
