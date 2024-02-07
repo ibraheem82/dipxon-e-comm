3450dd858bdc2a0854358f9a3de934751a82b2f6
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-07 18:15:40 +0100

    ## Commit message:
    **Add URL pattern for 'category-list' view displaying all categories.**
    
    This message clearly describes the change: adding a new URL pattern named "category-list" which points to the `category_list_view` in the `views` module. It mentions the purpose of the view (displaying all categories) for additional context.

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('shop/products', views.shop, name="shop"),
+    path('category/', views.category_list_view, name="category-list"),
     path('category/<cid>/', views.category_product_list_view, name="category-product-list"),
 ]
