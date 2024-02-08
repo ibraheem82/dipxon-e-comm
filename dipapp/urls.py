986d3bbde7bdb1f2c309e273c7097f79a149aa78
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-08 16:39:33 +0100

    ......

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('shop/products', views.shop, name="shop"),
+    path('product/<pid>', views.product_detail_view, name="product_detail"),
     path('category/', views.category_list_view, name="category-list"),
     path('category/<cid>/', views.category_product_list_view, name="category-product-list"),
 ]
