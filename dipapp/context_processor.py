51b3c761233b38221e1b41693cb61577aef2d3cc
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-23 19:57:58 +0100

    Commit message: ``` Refactor default context data retrieval in views
    - Refactor the default view context data retrieval logic
    - Initialize address variable with None and handle Address.DoesNotExist exception
    ```
    This commit refactors the default view to improve the retrieval logic for context data. It initializes the address variable with None and handles the Address.DoesNotExist exception to prevent potential errors when retrieving user addresses.

--- dipapp/context_processor.py
+++ dipapp/context_processor.py
@@ -4,9 +4,16 @@ from dipapp.models import Product, Category, Vendor, CartOrder, CartOrderItems, 
 
 def default(request):
     categories = Category.objects.all()
+    address = None  # Initialize address with None  
+    if request.user.is_authenticated:
+        try:
+            address = Address.objects.get(user=request.user) 
+        except Address.DoesNotExist:
+            pass
     
     return  {
         'categories': categories,
+        'address': address,
     }
 
      
