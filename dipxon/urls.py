1c9f458913ed597c4518fb7be5704fcebe2ada11
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-23 12:31:47 +0100

    path('ckeditor', ...: This specifies the URL pattern that starts with ckeditor. include('ckeditor_uploader.urls'): This includes the URLs provided by the ckeditor_uploader app. The ckeditor_uploader app is responsible for handling file uploads for CKEditor.

--- dipxon/urls.py
+++ dipxon/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dipapp.urls')),
     path('accounts/', include('accounts.urls')),
+    path('ckeditor', include('ckeditor_uploader.urls'))
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
