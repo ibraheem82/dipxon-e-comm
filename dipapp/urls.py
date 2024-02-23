e1f0fb197f91477970f825d947bb23a6046f6f75
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-23 11:57:30 +0100

    added url for the  tag....

--- dipapp/urls.py
+++ dipapp/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('product/<pid>', views.product_detail_view, name="product_detail"),
     path('category/', views.category_list_view, name="category-list"),
     path('category/<cid>/', views.category_product_list_view, name="category-product-list"),
+    
+    # Tags
+    path('products/tag/<slug:tag_slug/>', views.tag_list, name="tags"),
 ]
