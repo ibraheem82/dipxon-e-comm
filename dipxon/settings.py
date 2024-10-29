2a5fc15660366b6bac0657be27d1454a2a43f443
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-10-29 12:49:39 +0100

    database was configured and jazzmin unused codes was removed especially the commented ones...

--- dipxon/settings.py
+++ dipxon/settings.py
@@ -216,33 +216,10 @@ JAZZMIN_SETTINGS = {
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
 
-    ############
-    # Top Menu #
-    ############
-
-    # Links to put along the top menu
-    "topmenu_links": [
-
-        # Url that gets reversed (Permissions can be added)
-        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
-
-        # external url that opens in a new window (Permissions can be added)
-        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
-
-        # model admin to link to (Permissions checked against model)
-        {"model": "auth.User"},
-
-    ],
 
     #############
     # User Menu #
     #############
-
-    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
-    "usermenu_links": [
-        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
-        {"model": "auth.user"}
-    ],
 
     #############
     # Side Menu #
@@ -314,23 +291,6 @@ EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
-
-# CKEDITOR_CONFIGS = {
-#     'default': {
-#         'skin': 'mono',
-#         'codeSnippet_theme': 'monokai',
-#         'toolbar': 'all',
-#         'height': 300,
-#         'width': 300,
-#         'extraPlugins': ','.join(
-#             [
-#                 'codesnippet',
-#                 'widget',
-#                 'dialog'
-#             ]
-#         ),
-#     }
-# }
 
 
 
